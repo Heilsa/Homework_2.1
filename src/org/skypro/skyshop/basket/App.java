62db9b76699ed8a2f46c3f6c68effd1ce66bb3eb
Heilsa <katushenko.a@mail.ru>
2025-06-25 22:23:13 +0700

    HomeWork 2.1 rework(3)

--- src/org/skypro/skyshop/basket/App.java
+++ src/org/skypro/skyshop/basket/App.java
@@ -1,31 +1,24 @@
 package org.skypro.skyshop.basket;
 
-import org.skypro.skyshop.basket.DiscountedProduct;
-import org.skypro.skyshop.basket.FixPriceProduct;
-import org.skypro.skyshop.basket.SimpleProduct;
 import org.skypro.skyshop.product.Product;
 
 import java.util.ArrayList;
 import java.util.List;
 
-// Класс корзины
-class Basket {
-    private List<Product> products;
+class Main {
+    private List<Product> basket;
 
-    public Basket() {
-        this.products = new ArrayList<>();
+    // Конструктор для инициализации корзины
+    public Main(List<Product> basket) {
+        this.basket = basket;
     }
 
-    public void addProduct(Product product) {
-        this.products.add(product);
-    }
-
-    // Нестатический метод для печати содержимого корзины
+    // Нестатический метод для печати корзины
     public void printBasket() {
         int totalCost = 0;
         int specialCount = 0;
 
-        for (Product product : products) {
+        for (Product product : basket) {
             System.out.println(product.toString());
             totalCost += product.getPrice();
             if (product.isSpecial()) {
@@ -35,21 +28,20 @@ class Basket {
         System.out.println("Итого: " + totalCost);
         System.out.println("Специальных товаров: " + specialCount);
     }
-}
 
-public class App {
     public static void main(String[] args) {
-        // Создаем корзину
-        Basket basket = new Basket();
+        // Создаем список товаров (корзину)
+        List<Product> basket = new ArrayList<>();
+        basket.add(new SimpleProduct("Яблоко", 300));
+        basket.add(new FixPriceProduct("Молоко")); // фиксированная цена
+        basket.add(new DiscountedProduct("Киви", 200, 25)); // скидка 25%
+        basket.add(new FixPriceProduct("Апельсин")); // еще один фиксированный товар
+        basket.add(new DiscountedProduct("Гранат", 50, 15)); // скидка 15%
 
-        // Добавляем товары разных типов
-        basket.addProduct(new SimpleProduct("Яблоко", 300));
-        basket.addProduct(new FixPriceProduct("Молоко")); // фиксированная цена
-        basket.addProduct(new DiscountedProduct("Киви", 200, 25)); // скидка 25%
-        basket.addProduct(new FixPriceProduct("Апельсин")); // еще один фиксированный товар
-        basket.addProduct(new DiscountedProduct("Гранат", 50, 15)); // скидка 15%
+        // Создаем объект Main с корзиной
+        Main shoppingCart = new Main(basket);
 
-        // Выводим содержимое корзины
-        basket.printBasket();
+        // Вызываем метод для печати корзины
+        shoppingCart.printBasket();
     }
 }
